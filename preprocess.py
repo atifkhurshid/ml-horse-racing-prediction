10073387362bd14dbc0b16a6c847c5d2cdfddc9d
Atif Khurshid <1155085457@link.cuhk.edu.hk>
2018-04-18 18:13:53 +0800

    Preprocessing complete

--- preprocess.py
+++ preprocess.py
@@ -1,54 +1,128 @@
 import pandas as pd
 import seaborn
-import numpy as np
 
 seaborn.set()
-np.set_printoptions(precision=3, linewidth=100, suppress=True)
 
+print ('Reading race-result-horse.csv...')
 df = pd.read_csv('data/race-result-horse.csv',
                  na_values=['DISQ', 'DNF', 'FE', 'PU', 'TNP', 'UR', 'VOID', 'WR', 'WV', 'WV-A', 'WX', 'WX-A', 'nan'])
 
-df = df.dropna(subset=['finishing_position'])
+print ('Reading race-result-race.csv...')
+df2 = pd.read_csv('data/race-result-race.csv')
 
+print ("Dropping non-integer finishing-positions...")
+df = df.dropna(subset=['finishing_position'])
 df['finishing_position'] = df['finishing_position'].replace([' DH'], '', regex=True)
 
-
-df['recent_6_runs'] = 'nan'
+print ("Creating new columns...")
+df['recent_6_runs'] = 'NA'
 df['recent_ave_rank'] = 7
+df['horse_index'] = 'NA'
+df['jockey_index'] = 'NA'
+df['trainer_index'] = 'NA'
+df['jockey_ave_rank'] = 7
+df['trainer_ave_rank'] = 7
+df['race_distance'] = 'NA'
+df['race_course'] = 'NA'
+df['race_class'] = 'NA'
+df['track'] = 'NA'
+df['track_condition'] = 'NA'
+df['horse_win'] = 0
+df['horse_rank_top_3'] = 0
+df['horse_rank_top_50_percent'] = 0
+
+print ("Generating lists of races, horses, jockeys and trainers...")
+race_ids = []
 id_list = []
+jockey_list = []
+trainer_list = []
+idx = 0
+
+for id in df['race_id']:
+    if id not in race_ids:
+        race_ids.append(id)
+
 for id in df['horse_id']:
     if id not in id_list:
         id_list.append(id)
+
+for name in df['jockey']:
+    if name not in jockey_list:
+        jockey_list.append(name)
+
+for name in df['trainer']:
+    if name not in trainer_list:
+        trainer_list.append(name)
+
+print ("Number of horses: ", len(id_list))
+print ("Number of jockeys: ", len(jockey_list))
+print ("Number of trainers: ", len(trainer_list))
+
+print ("Adding racecourse information...")
+for id in race_ids:
+    df.loc[df.race_id == id, 'race_distance'] = df2.loc[df2.race_id == id, 'race_distance'].item()
+    df.loc[df.race_id == id, 'race_course'] = df2.loc[df2.race_id == id, 'race_course'].item()
+    df.loc[df.race_id == id, 'race_class'] = df2.loc[df2.race_id == id, 'race_class'].item()
+    df.loc[df.race_id == id, 'track'] = df2.loc[df2.race_id == id, 'track'].item()
+    df.loc[df.race_id == id, 'track_condition'] = df2.loc[df2.race_id == id, 'track_condition'].item()
+
+print ("Adding horse statistics (index, rank, recent avg and results)...")
 for id in id_list:
+    df.loc[df['horse_id'] == id, 'horse_index'] = idx
+    idx += 1
+
     breaker = '/'
     recent = []
     for index, record in df[df['horse_id'] == id].iterrows():
-        df.loc[index, 'recent_6_runs'] = breaker.join(recent)
+        pos = int(record.finishing_position)
+        if pos == 1:
+            df.loc[index, 'horse_win'] = 1
+            df.loc[index, 'horse_rank_top_3'] = 1
+            df.loc[index, 'horse_rank_top_50_percent'] = 1
+        elif pos <= 3:
+            df.loc[index, 'horse_rank_top_3'] = 1
+            df.loc[index, 'horse_rank_top_50_percent'] = 1
+        else:
+            num_horses = max(map(float,df.loc[df['race_id'] == record.race_id, 'finishing_position']))
+            if float(pos) / num_horses <= 0.5:
+                df.loc[index, 'horse_rank_top_50_percent'] = 1
+
         if (len(recent)) != 0:
             df.loc[index, 'recent_ave_rank'] = sum(map(float,recent))/float(len(recent))
+
+        df.loc[index, 'recent_6_runs'] = breaker.join(recent)
         recent.append(record.finishing_position)
         if len(recent) > 6:
             recent = recent[1:]
 
-print(df)
+print ("Adding jockey statistics (index, avg rank)...")
+idx = max(idx, 20000)
+for name in jockey_list:
+    df.loc[df.jockey == name, 'jockey_index'] = idx
+    idx += 1
+    jockey_record = df.loc[df.jockey == name]
+    jockey_position = jockey_record.loc[df.race_id <= '2016-327', 'finishing_position']
+    if len(jockey_position) != 0:
+        df.loc[df.jockey == name, 'jockey_ave_rank'] = \
+            sum(map(float, jockey_position))/float(len(jockey_position))
+
+print ("Adding trainer statistics (index, avg rank)...")
+idx = max(idx, 40000)
+for name in trainer_list:
+    df.loc[df.trainer == name, 'trainer_index'] = idx
+    idx += 1
+    trainer_record = df.loc[df.trainer == name]
+    trainer_position = trainer_record.loc[df.race_id <= '2016-327', 'finishing_position']
+    if len(trainer_position) != 0:
+        df.loc[df.trainer == name, 'trainer_ave_rank'] = \
+            sum(map(float, trainer_position))/float(len(trainer_position))
 
 
-# Give a unique index to each horses, where “which horse has which index” is not restricted. Similarly, a
-#unique index should be assigned to each jockey, and a unique index should be assigned to each trainer.
+print ("Creating training.csv and testing.csv files...")
+df_train = df.loc[df.race_id <= '2016-327']
+df_test = df.loc[df.race_id > '2016-327']
 
+df_train.to_csv('data/training.csv')
+df_test.to_csv('data/testing.csv')
 
-#Add a column named jockey_ave_rank to the dataframe that records the average rank of the jockey
-#in the training data. Similarly, add a column named trainer_ave_rank that records the average rank
-#of the trainer in the training data. Note that if a jockey or a trainer doesn’t appear in the training
-#data, set the average rank of the jockey to be 7.
-
-# Read the distance information in race-result-race.csv and add a column to the dataframe race_distance
-#for each entry in race-result-horse.csv.
-
-
-# ADD THREE COLUMNS Top1, Top3 or TOp 50% - 1/0
-
-#Split the dataframe after all the above pre-processing into two parts, and save them to files. The
-#training data should be saved as training.csv and the testing data be saved as testing.csv.
-
-
+print ("Complete!")
